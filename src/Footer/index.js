4055d9421995a5d98b47e235eb66429d2913f944
Trish Whetzel <plwhetzel@gmail.com>
2019-08-30 07:44:48 -0700

    add logos, #63

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-
+import Button from '@material-ui/core/Button';
 
 var style = {
-    backgroundColor: "#E7F7F9",
+    backgroundColor: "#A9A9A9",
     textAlign: "center",
     padding: "20px",
-    height: "60px",
+    height: "80px",
     width: "100%",
 }
 
@@ -17,6 +17,10 @@ var linkStyle = {
     paddingRight: 4,
 }
 
+var button = {
+    backgroundColor: "#A9A9A9",
+    border: 'none',
+}
 
 function Footer() {
     const textDivider = "|";
@@ -40,6 +44,12 @@ function Footer() {
                 <Link target="_blank" href="https://www.ebi.ac.uk/about/terms-of-use" style={linkStyle}>
                     Terms of use
                     </Link>
+                <Button style={button} target="_blank" href="https://www.ebi.ac.uk/">
+                    <img src={process.env.PUBLIC_URL + '/images/EMBL_EBI_Logo_white.png'} alt="embl-ebi" />
+                </Button>
+                <Button style={button} target="_blank" href="https://www.genome.gov/">
+                    <img src={process.env.PUBLIC_URL + '/images/NHGRI_NIH_logo.png'} alt="nih" />
+                </Button>
             </Typography>
         </div>
     )
