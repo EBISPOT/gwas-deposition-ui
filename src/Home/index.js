a759db111a14ebe4e2569dd65138644ec0385368
Trish Whetzel <plwhetzel@gmail.com>
2019-09-24 13:15:15 -0700

    update sytle and text, #101

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,67 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from '@material-ui/core/Container';
 
 import { UserConsumer } from '../user-context';
 import { AuthConsumer } from '../auth-context';
 
 import PublicationsMatTable from '../PublicationsMatTable';
-import { Paper, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-class Home extends Component {
+
+const styles = theme => ({
+    fontStyle: {
+        fontWeight: 600,
+    }
+});
+
+
+class Home extends React.Component {
     render() {
+        const { classes } = this.props;
         return (
             <Container>
-                <Paper>
+                <Grid container
+                    direction="column"
+                    justify="space-evenly"
+                    alignItems="center"
+                    spacing={4}>
+                    <Grid item>
+                        <Typography gutterBottom variant="h5" className={classes.fontStyle}>
+                            Welcome to the GWAS Catalog submission page
+                        </Typography>
+                    </Grid>
                     <Grid container
                         direction="column"
-                        justify="space-evenly"
-                        alignItems="center">
-                        <Grid item>
-                            <Typography gutterBottom variant="h5">
-                                Welcome to the GWAS Catalog submission page
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Grid item xs={11}>
+                            <Typography gutterBottom variant="body1">
+                                In the search bar below, you can search all GWAS Catalog publications,
+                                including those in the curation queue. If you are an author of the publication
+                                and have available summary statistics, please click to “Create submission”.
+                                For additional information please read the documentation. If you cannot find your
+                                publication, or need further help, please e-mail <a href="mailto:gwas-info@ebi.ac.uk?subject=Deposition submission help request">gwas-info@ebi.ac.uk</a>.
                             </Typography>
-                        </Grid>
-                        <Grid container spacing={3}>
-                            <Grid item xs>
-                                <Paper></Paper>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <Typography gutterBottom variant="body1">
-                                    In the table below, you can view all eligible GWAS Catalog publications, including those in the curation queue.
-                                    If you are an author of one of these publications and have available summary statistics, please click on
-                                    the publication PMID to start your submission.
-                                    For additional information please read the documentation [link to “How to submit” page].
-                            </Typography>
-
-                            </Grid>
-                            <Grid item xs>
-                            </Grid>
                         </Grid>
                     </Grid>
 
-                </Paper>
 
-
-                {/* <UserConsumer>
+                    {/* <UserConsumer>
                     {({ username }) => <h1>Welcome {username}!</h1>}
                 </UserConsumer> */}
 
-                {/* <h4> JWTToken: {this.props.token}</h4> */}
+                    {/* <h4> JWTToken: {this.props.token}</h4> */}
 
-                {/* Alternative option to get info from Context with just "export default Home" */}
-                {/* <AuthConsumer>
+                    {/* Alternative option to get info from Context with just "export default Home" */}
+                    {/* <AuthConsumer>
                     {({ isAuthenticated }) => <h4> Login State: {isAuthenticated.toString()}</h4>}
                 </AuthConsumer> */}
 
-                <PublicationsMatTable />
+                    <Grid item>
+                        <PublicationsMatTable />
+                    </Grid>
+
+                </Grid>
             </Container>
         )
     }
 }
 // export default Home
-
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+Home = withStyles(styles)(Home)
 
 export default () => (
     <AuthConsumer>
