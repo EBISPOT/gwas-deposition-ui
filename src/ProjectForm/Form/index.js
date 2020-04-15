a12d04b1e0be6feedead12692f2792abdca83823
Trish Whetzel <plwhetzel@gmail.com>
2020-04-15 19:29:22 +0100

    add url field validation

--- src/ProjectForm/Form/index.js
+++ src/ProjectForm/Form/index.js
@@ -209,6 +209,32 @@ const MyEnhancedForm = withFormik({
         }
 
         /**
+         * Journal URL
+         */
+        // From:  https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        if (values.url) {
+            if (!pattern.test(values.url)) {
+                errors.url = "Add valid URL"
+            }
+        }
+
+        /**
+        * Journal URL
+        */
+        if (values.preprintServerDOI) {
+            if (!pattern.test(values.preprintServerDOI)) {
+                errors.preprintServerDOI = "Add valid URL"
+            }
+        }
+
+
+        /**
          * First Author Validation for fields:
          * First name
          * Last name
